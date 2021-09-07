0535c52ef363f7ecf50ba850c1ef9fd84982b139
Craig Baldry <cbaldry@our.ecu.edu.au>
2021-09-07 15:41:15 +0800

    fix up graph and seperate data for testing

--- model/timeseries.py
+++ model/timeseries.py
@@ -10,6 +10,8 @@ import json
 
 json_path = "heart_rate.json"
 
+# Retrieved from kaggle
+# TODO: share link where data was retrieved from
 if not exists(json_path):
     zip_path = "heart_rate.json.zip"
     zip_file = ZipFile(zip_path)
@@ -19,26 +21,41 @@ with open(json_path) as f:
     d = json.load(f)
 
 # Normalise Json
-df1 = pd.json_normalize(d)
+df = pd.json_normalize(d)
 
 # Convert dateTime object
-df1['dateTime'] = pd.to_datetime(df1['dateTime'])
+df['dateTime'] = pd.to_datetime(df['dateTime'])
 
 # Remove confidence
-df1 = df1.drop(['value.confidence'], axis=1)
+df = df.drop(['value.confidence'], axis=1)
 
 # Show the data we are working with
 # df1.shape
 # df1.info()
 # print(df1.head(-10))
 
-df1.sort_values(by="dateTime")
-df1 = df1.set_index('dateTime')
+df.sort_values(by="dateTime")
+df = df.set_index('dateTime')
 
 # Drop any nans and round to zero
-df1 = df1.resample('1Min').mean().dropna().round(0)
-print(df1.head(5))
+df = df.resample('1Min').mean().dropna().round(0)
+print(df.head(5))
 
-df1.plot()
-plt.show()
+# Split data for testing
+df_test_data = df.loc[(df.index > '2020-01-12 00:00:00') & (df.index <= '2020-01-12 23:59:59')]
+df = df.loc[(df.index < '2020-01-12 00:00:00') | (df.index > '2020-01-12 23:59:59')]
 
+# Visualising training and testing data
+datasets = [df, df_test_data]
+for idx, dfs in enumerate(datasets):
+    fig, axs = plt.subplots(figsize=(10, 6))
+    axs.plot(dfs['value.bpm'])
+    axs.tick_params(axis='x', rotation=60)
+    axs.set(ylabel='Heart Rate (BPM)', xlabel="Date")
+    plt.tight_layout()
+    plt.show()
+    fig.savefig(str(idx+1) + "_data_fig.png")
+
+# TODO: correlation
+# Make a correlation coefficient
+# print(df.corr())
